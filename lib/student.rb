4d31e9134d8a574ea09d58abf317e5898038d7ef
Wendy Harris <wendyv510@hotmail.com>
2020-01-03 18:51:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
      end
   end 
   
-  def self.first_x_students_in_grade_10(num)
+  def self.first_X_students_in_grade_10(num)
     sql = "SELECT *
            FROM students 
            WHERE grade = 10
